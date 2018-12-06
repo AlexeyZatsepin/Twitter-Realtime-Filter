4682b3a796e1e8ceeaa020ec5b8b9a291028c47d
Alexey Zatsepin <alexzatsepin7@gmail.com>
2018-12-07 00:16:02 +0200

    fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.externals import joblib
 
 used_features = ['screen_name_binary', 'name_binary', 'description_binary', 'status_binary', 'listed_count_binary',
                  'verified', 'followers_count', 'friends_count', 'statuses_count']
-bag_of_words_bot = 'bot','b0t'
+bag_of_words_bot = 'bot', 'b0t'
 
 
 def contains(str):
@@ -36,67 +36,33 @@ if __name__ == '__main__':
 
     class MyListener(StreamListener):
         def on_data(self, data):
-            twitt = json.loads(data)
-            print(twitt["user"])
-            user = twitt["user"]
-            # {'id': 1066252633269256192,
-            # 'id_str': '1066252633269256192',
-            # 'name': 'Mobile Apps & Games',
-            # 'screen_name': 'KaracaMobile',
-            # 'location': None,
-            # 'url': 'https://play.google.com/store/apps/developer?id=Karaca',
-            # 'description': None,
-            # 'translator_type': 'none', pop
-            # 'protected': False, pop
-            # 'verified': False, '
-            # followers_count': 247,
-            # 'friends_count': 1318,
-            # 'listed_count': 1,
-            # 'favourites_count': 193,
-            # 'statuses_count': 74,
-            # 'created_at': 'Sat Nov 24 08:50:12 +0000 2018',
-            # 'utc_offset': None, pop
-            # 'time_zone': None, pop
-            # 'geo_enabled': False, pop
-            # 'lang': 'en',
-            # 'contributors_enabled': False, pop
-            # 'is_translator': False, pop
-            # 'profile_background_color': 'F5F8FA',
-            # 'profile_background_image_url': '',
-            # 'profile_background_image_url_https': '',
-            # 'profile_background_tile': False,
-            # 'profile_link_color': '1DA1F2',
-            # 'profile_sidebar_border_color': 'C0DEED',
-            # 'profile_sidebar_fill_color': 'DDEEF6',
-            # 'profile_text_color': '333333',
-            # 'profile_use_background_image': True,
-            # 'profile_image_url': 'http://pbs.twimg.com/profile_images/1066253374948065280/omYTK152_normal.jpg',
-            # 'profile_image_url_https': 'https://pbs.twimg.com/profile_images/1066253374948065280/omYTK152_normal.jpg',
-            # 'profile_banner_url': 'https://pbs.twimg.com/profile_banners/1066252633269256192/1543049627',
-            # 'default_profile': True,
-            # 'default_profile_image': False,
-            # 'following': None, pop
-            # 'follow_request_sent': None, pop
-            # 'notifications': None} pop
-            input_data = pd.DataFrame(np.array([used_features,
-                                                [contains(user["screen_name"]),
-                                                 contains(user["name"]),
-                                                 contains(user["description"]),
-                                                 (user["statuses_count"] > 0),
-                                                 (user["listed_count"] < 20000),
-                                                 user["verified"],
-                                                 user["followers_count"],
-                                                 user["friends_count"],
-                                                 user["statuses_count"]]]))
-            result = final_model.predict(input_data)
-            return True
+            try:
+                twitt = json.loads(data)
+                print("Message " + twitt["text"])
+                user = twitt["user"]
+                print("Username " + user["name"])
+                data = [contains(user["screen_name"]),
+                                           contains(user["name"]),
+                                           contains(user["description"]),
+                                           (user["statuses_count"] > 0),
+                                           (user["listed_count"] < 20000),
+                                           user["verified"],
+                                           user["followers_count"],
+                                           user["friends_count"],
+                                           user["statuses_count"]]
+                input_data = pd.DataFrame([data])
+                result = final_model.predict(input_data)
+                print("Bot :" + str(result[0]))
+                return True
+            except Exception as e:
+                print(e)
 
         def on_error(self, status):
             print(status)
             return True
 
 
-    def listen(tags=['#android', "#java", "#kotlin", "#spring"]):
+    def listen(tags=['#android', "#java", "#kotlin", "#spring",""]):
         twitter_stream = Stream(auth, MyListener())
         twitter_stream.filter(track=tags)
 
