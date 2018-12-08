e179518610633fc35014e8b3e9c4a7f2189f425b
Alexey Zatsepin <alexzatsepin7@gmail.com>
2018-12-08 13:54:43 +0200

    add mobile client

---
+++ twitter_mobile/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Real time Twitter dashbord',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PhotoList(),
+    );
+  }
+}
+
+class PhotoList extends StatefulWidget {
+  @override
+  PhotoListState createState() => PhotoListState();
+}
+
+class PhotoListState extends State<PhotoList> {
+  List<Twitt> list = [];
+  IOWebSocketChannel channel;
+
+  @override
+  void initState() {
+    super.initState();
+    channel = new IOWebSocketChannel.connect("ws://192.168.1.25:1234/svc/websockets");
+
+    channel.stream.listen((message) => setState(() => list.add(Twitt.fromJsonMap(message))));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    channel.sink.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Real time Twitter dashbord"),
+      ),
+      body: Center(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) => _makeElement(index),
+        ),
+      ),
+    );
+  }
+
+  Widget _makeElement(int index) {
+    if (index >= list.length) {
+      return null;
+    }
+
+    return Container(
+        padding: EdgeInsets.all(5.0),
+        child: Padding(
+          padding: EdgeInsets.only(top: 200.0),
+          child: Column(
+            children: <Widget>[
+              Image.network(list[index].url, width: 150.0, height: 150.0),
+              Text(list[index].text),
+            ],
+          ),
+        ));
+  }
+}
+
+class Twitt {
+  final String text;
+  final String url;
+
+  Twitt.fromJsonMap(Map map)
+      : text = map['title'],
+        url = map['url'];
+}
